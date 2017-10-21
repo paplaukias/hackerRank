79bdc3bb90bf8cb8474ec1b9ef118d50a377420c
Domantas Cibas <domantas@nectarsun.com>
2017-10-21 23:42:00 +0300

    adds recusive division

--- binaries.c
+++ binaries.c
@@ -6,8 +6,23 @@
 #include <limits.h>
 #include <stdbool.h>
 
-int count_binary_ones(int dec) {
-  return dec;
+static int one_counter = 0, max = 0;
+
+int recursive_division(int in) {
+  int result, remainder = in % 2;
+  if(in > 1) {
+    result = recursive_division(in / 2);
+  } else {
+    result = in;
+  }
+  if(remainder == 1) {
+    one_counter++;
+    if(one_counter > max) max = one_counter;
+  } else {
+    one_counter = 0;
+  }
+  // printf("%d", remainder);
+  return result;
 }
 
 int main() {
@@ -15,6 +30,7 @@ int main() {
   int result;
   scanf("%d",&n);
   if(n < 1 || n > 1000000) return 0;
-  printf("%d\r\n", count_binary_ones(n));
+  recursive_division(n);
+  printf("\r\n%d\r\n", max);
   return 0;
 }
